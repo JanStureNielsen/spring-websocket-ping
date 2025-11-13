dfb19c6511f985ee3f38417855e18b111232b5b5
Jan Nielsen <jan.sture.nielsen@gmail.com>
2025-11-13 21:45:06 +0000

    Polish -- imports.

--- client/src/main/java/sample/spring/websocket/client/Application.java
+++ client/src/main/java/sample/spring/websocket/client/Application.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.HdrHistogram.Histogram;
